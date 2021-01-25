f5490b3130c74296bd16e68ecc9b28f192eded8b
Michael Walker <Michael.e.walker.87@gmail.com>
2021-01-25 16:02:28 -0700

    Start with vue

---
+++ src/components/TheContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped>
+  section{
+    padding: 2em;
+    background-color: blue;
+  } 
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <TheHeader msg="Welcome Nathan"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TheHeader from '@/components/TheHeader.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    TheHeader
+  }
+}
+</script>
